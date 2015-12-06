9bbddfcab5c33760da83cec7eecd96be06dc0366
David Yu <lewen.yu@gmail.com>
2015-12-05 22:47:14 -0800

    spotlight to follow ball and player

---
+++ Assets/DynamicBackground.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DynamicBackground : MonoBehaviour {
+	private Renderer bg;
+	private GameObject player;
+	private GameObject ball;
+
+	// Use this for initialization
+	void Start () {
+		bg = GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( player == null ) player = GameObject.FindGameObjectWithTag( "Player" );
+		if( ball == null ) ball = GameObject.FindGameObjectWithTag( "Ball" );
+		bg.material.SetVector("_PlayerPosition", Camera.main.WorldToScreenPoint( player.transform.position ) );
+		bg.material.SetVector("_BallPosition", Camera.main.WorldToScreenPoint( ball.transform.position ) );
+	}
+}
