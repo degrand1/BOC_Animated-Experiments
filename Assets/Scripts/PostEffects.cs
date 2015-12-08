bb6c89125d2983f500acd7fb9c2a441768f366af
David Yu <lewen.yu@gmail.com>
2015-12-07 19:53:05 -0800

    first-pass bloom shader

---
+++ Assets/Scripts/PostEffects.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PostEffects : MonoBehaviour {
+	public Shader postFXShader = null;
+	private Material mat;
+
+	// Use this for initialization
+	void Start () {
+		if (postFXShader)
+		{
+			mat = new Material(postFXShader);
+			mat.name = "PostFXMaterial";
+			mat.hideFlags = HideFlags.HideAndDontSave;
+		}
+		
+		else
+		{
+			Debug.LogWarning(gameObject.name + ": Post FX Shader is not assigned. Disabling...", this.gameObject);
+			enabled = false;
+		}
+	}
+	
+	void OnRenderImage(RenderTexture src, RenderTexture dst)
+	{
+		if (postFXShader && mat)
+		{
+			Graphics.Blit(src, dst, mat);
+		}
+		else
+		{
+			Graphics.Blit(src, dst);
+			Debug.LogWarning(gameObject.name + ": Post FX Shader is not assigned. Disabling...", this.gameObject);
+			enabled = false;
+		}
+	}
+
+	void OnDisable()
+	{
+	}
+}
