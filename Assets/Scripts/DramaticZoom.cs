4d1fec6e94d50234646d5e8a88d4557f2cf6e411
David Yu <lewen.yu@gmail.com>
2015-12-09 20:54:43 -0800

    first pass slo-mo

---
+++ Assets/Scripts/DramaticZoom.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DramaticZoom : MonoBehaviour {
+	private GameObject Ball;
+	private float acc = 1;
+	public float duration = 1;
+
+	// Use this for initialization
+	void Start () {
+		Ball = GameObject.FindGameObjectWithTag ("Ball");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( acc < 1 ) {
+			acc += Time.deltaTime;
+			Time.timeScale = Mathf.Clamp( Mathf.Abs( Easing.Quadratic.InOut( acc/duration ) - 0.5f ) + 0.5f, 0, 1 );
+		}
+
+		// for debug use only DELETEME
+		if (Input.GetKey (KeyCode.LeftShift) && acc >= 1) {
+			acc = 0;
+		}
+	}
+}
---
+++ Assets/Scripts/Easing.cs
@@ -0,0 +1,18 @@
+﻿public class Easing {
+	public class Quadratic {
+		public static float Out( float t ) {
+			return -t * ( t - 2 );
+		}
+
+		public static float In( float t ) {
+			return t * t;
+		}
+
+		public static float InOut( float t ) {
+			if ( t < 0.5 )
+				return 2 * t * t;
+			else
+				return -0.5f * ( ( 2*t-1 ) * ( 2*( t-1 )-2 ) - 1 );
+		}
+	}
+}
